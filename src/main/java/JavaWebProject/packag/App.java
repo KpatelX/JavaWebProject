6b7f366d1e3ab463ab2395245f440fae6a0018e6
KartikPatel1997 <128299611+KartikPatel1997@users.noreply.github.com>
2025-12-30 09:32:35 +0530

    remove sop

--- src/main/java/JavaWebProject/packag/App.java
+++ src/main/java/JavaWebProject/packag/App.java
@@ -10,6 +10,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Count this as commit" );
-        System.out.println( "after configuration wiith jenkis" );
     }
 }
